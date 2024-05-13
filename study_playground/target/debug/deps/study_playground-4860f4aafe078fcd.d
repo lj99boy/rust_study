bc1f6431693cf5419d21113ba1df3597b4e1b182
lj99boy <lj99boy@hotmail.com>
2024-05-14 00:34:48 +0800

    init

---
+++ study_playground/target/debug/deps/study_playground-4860f4aafe078fcd.d
@@ -0,0 +1,5 @@
+/home/lj99boy/shikodo/study_space/rust_folder/rust_study/study_playground/target/debug/deps/libstudy_playground-4860f4aafe078fcd.rmeta: src/main.rs
+
+/home/lj99boy/shikodo/study_space/rust_folder/rust_study/study_playground/target/debug/deps/study_playground-4860f4aafe078fcd.d: src/main.rs
+
+src/main.rs:
---
+++ study_playground/target/debug/deps/study_playground-ff3cd418a80d47b0.d
@@ -0,0 +1,5 @@
+/home/lj99boy/shikodo/study_space/rust_folder/rust_study/study_playground/target/debug/deps/libstudy_playground-ff3cd418a80d47b0.rmeta: src/main.rs
+
+/home/lj99boy/shikodo/study_space/rust_folder/rust_study/study_playground/target/debug/deps/study_playground-ff3cd418a80d47b0.d: src/main.rs
+
+src/main.rs:
